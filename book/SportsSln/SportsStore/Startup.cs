2184cb9d0526db8f15540eba2256213df231d48b
s-ly <sergeylysov@outlook.com>
2025-08-24 20:04:14 +0300

    final old book

--- book/SportsSln/SportsStore/Startup.cs
+++ book/SportsSln/SportsStore/Startup.cs
@@ -11,40 +11,39 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
 
-namespace SportsStore
-{
-    public class Startup
-    {
-        public Startup(IConfiguration config)
-        {
+namespace SportsStore {
+    public class Startup {
+
+        public Startup(IConfiguration config) {
             Configuration = config;
         }
 
         private IConfiguration Configuration { get; set; }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
-            services.AddDbContext<StoreDbContext>(opts =>
-            {
+
+            services.AddDbContext<StoreDbContext>(opts => {
                 opts.UseSqlServer(
                     Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+
             app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("pagination",
+                    "Products/Page{productPage}",
+                    new { Controller = "Home", action = "Index" });
                 endpoints.MapDefaultControllerRoute();
             });
+
             SeedData.EnsurePopulated(app);
         }
     }
