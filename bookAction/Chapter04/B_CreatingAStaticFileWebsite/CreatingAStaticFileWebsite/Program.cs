700cc1d41ef2d014d96b8997e19ebc9e5adb8f9c
Sergey Lysov <sergeylysov@MacBook-Pro-Sergey.local>
2025-08-26 01:56:24 +0300

    WelcomePage

--- bookAction/Chapter04/B_CreatingAStaticFileWebsite/CreatingAStaticFileWebsite/Program.cs
+++ bookAction/Chapter04/B_CreatingAStaticFileWebsite/CreatingAStaticFileWebsite/Program.cs
@@ -1,6 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
 
+app.UseWelcomePage("/");
+app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
