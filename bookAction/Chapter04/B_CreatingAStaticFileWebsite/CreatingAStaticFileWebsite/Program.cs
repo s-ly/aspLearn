eae10e757aba77676946d8fbb642eb89f7fff80f
Sergey Lysov <sergeylysov@MacBook-Pro-Sergey.local>
2025-08-28 00:42:03 +0300

    map error

--- bookAction/Chapter04/B_CreatingAStaticFileWebsite/CreatingAStaticFileWebsite/Program.cs
+++ bookAction/Chapter04/B_CreatingAStaticFileWebsite/CreatingAStaticFileWebsite/Program.cs
@@ -1,11 +1,25 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-WebApplication app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-app.UseWelcomePage("/");
-app.UseDeveloperExceptionPage();
-app.UseStaticFiles();
-app.UseRouting();
+app.UseExceptionHandler("/error");
 
-app.MapGet("/", () => "Hello World!");
+// normally this would only be used in Production, and you'd rely on the DeveloperExceptionPage middleware
+// that's automatically added by WebApplication in Development
+// e.g.:
+if (app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.MapGet("/", () => BadService.GetValues());
+app.MapGet("/error", () => "Sorry, there was a problem processing your request");
 
 app.Run();
+
+class BadService
+{
+    public static string? GetValues()
+    {
+        throw new Exception("Oops, something bad happened!");
+    }
+}
