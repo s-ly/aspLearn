aac7afdf8b2ae890865812b3d2540d4b31ea89fd
s-ly <sergeylysov@outlook.com>
2025-08-28 14:52:53 +0300

    log

--- my/Proto/Protocol/Program.cs
+++ my/Proto/Protocol/Program.cs
@@ -1,42 +1,29 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Регистрация службы логирования HTTP-трафика.
+// Логируем всё: запросы и ответы.
+builder.Services.AddHttpLogging(opts => {
+    opts.LoggingFields =
+        HttpLoggingFields.Request |         // Запросы (метод, путь, заголовки)
+        HttpLoggingFields.Response |        // Ответы (статус, заголовки)
+        HttpLoggingFields.RequestBody |     // Тело запроса (если нужно)
+        HttpLoggingFields.ResponseBody;     // Тело ответа (важно!)
+});
+
+// Фильтр логирования: выводим информационные сообщения и выше
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "Test - OK");
-app.MapGet("/obj2", () => 42);
-app.MapGet("/obj", () => new Patient("Tony"));
-app.MapGet("/obj3", () => new Person("Nika")); // не отображается (нет свойств)
-app.MapGet("/obj4", () => new Human("Obyvan"));
-app.MapGet("/obj5", () => new PatientStruct("Grom"));
-app.MapGet("/obj6", () => new List<string> { "q", "w", "e" }); // возвращает массив
+// Активация промежуточного ПО в Development
+if (app.Environment.IsDevelopment()) {
+    app.UseHttpLogging();
+}
+
+app.MapGet("/", () => new Patient("Tony"));
 
 app.Run();
 
-// неизменяемый
-// immutable DTO (объектов передачи данных)
-// как класс, только конструктор и свойства создаются автоматом.
 public record Patient(string name);
-
-// неподходет, так-как нет свойств
-public class Person {
-    public string name;
-    public Person(string n) {
-        name = n;
-    }
-}
-
-// работает, но пришлось писать конструктор и свойство вручную
-public class Human {
-    public string Name { get; set; }
-    public Human(string name) {
-        Name = name;
-    }
-}
-
-// структура тоже работает (но изменяемая и есть особенности, копирование при передачи).
-public struct PatientStruct {
-    public string Name { get; set; }
-    public PatientStruct(string name) {
-        Name = name;
-    }
-}
