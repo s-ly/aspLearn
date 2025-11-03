2f0d560a4cbe7e1779601a5582ba6a2d742c21fc
s-ly <sergeylysov@outlook.com>
2025-11-03 04:12:23 +0300

    bot

--- my/Bot/Program.cs
+++ my/Bot/Program.cs
@@ -1,15 +1,15 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
-// для управления асинхронными операциями.
-using var cts = new CancellationTokenSource();
-
+using var cts = new CancellationTokenSource();  // для управления асинхронными операциями.
 var bot = new TelegramBotClient("1926895541:AAEzal5n00cdISLZckMuQNlxOf6Ak0yGt5g", cancellationToken: cts.Token);
 var me = await bot.GetMe();
 
 // Бот подписывается на событие OnMessage, которое вызывается при получении нового сообщения.
 bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
 
 Console.WriteLine($"Привет, я пользователь {me.Id} и моё имя {me.FirstName}.");
 Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
@@ -20,6 +20,27 @@ cts.Cancel();
 async Task OnMessage(Message msg, UpdateType type)
 {
     if (msg.Text is null) return;
-    Console.WriteLine($"Получен {type} '{msg.Text}' в {msg.Chat}");
-    await bot.SendMessage(msg.Chat, $"{msg.From} сказал: {msg.Text}");
+    if (msg.Text == "/Start")
+    {
+        await bot.SendMessage(msg.Chat,
+        "Welcome Pick one direction",
+        replyMarkup: new InlineKeyboardButton[] { "Left", "Right" });
+    }
+    else
+    {
+        Console.WriteLine($"Получен {type} '{msg.Text}' в {msg.Chat}");
+        await bot.SendMessage(msg.Chat, $"{msg.From} сказал: {msg.Text}");
+    }
+}
+
+// Метод, который обрабатывает другие типы обновлений, полученных ботом.
+// При нажатии кнопки ваш бот получает обновление типа CallbackQuery , 
+// которое не является простым сообщением. Поэтому оно будет обработано OnUpdate.
+async Task OnUpdate(Update update)
+{
+    if (update is { CallbackQuery: { } query })
+    {
+        await bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
+        await bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
+    }
 }
